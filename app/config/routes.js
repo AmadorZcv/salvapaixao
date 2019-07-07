2a2a39e7f0108b9384a550f15e3e086ea79149e6
AmadorZcv <lucasamadorzcv@gmail.com>
2019-07-06 21:15:40 -0300

    Rotas

--- app/config/routes.js
+++ app/config/routes.js
@@ -8,6 +8,15 @@ import {
 import Home from "../screens/Home";
 import { Icon } from "react-native-elements";
 import { Color, TextStyle } from "../styles";
+import SBancada from "../screens/SBancada";
+import SMetais from "../screens/SMetais";
+import SP from "../screens/SP";
+import SPEspeciais from "../screens/SPEspeciais";
+import SPintura from "../screens/SPintura";
+import SPLimpeza from "../screens/SPLimpeza";
+import SPResistente from "../screens/SPResistente";
+import SQuina from "../screens/SQuina";
+import SRalo from "../screens/SRalo";
 
 const HomeStack = createStackNavigator(
   {
@@ -16,6 +25,204 @@ const HomeStack = createStackNavigator(
       navigationOptions: ({ navigation }) => {
         return {
           headerTitle: "Salva Paixão",
+          headerLeft: null,
+          headerRight: (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Carrinho")}
+              style={{ paddingRight: 10 }}
+            >
+              <Icon
+                name={"ios-cart"}
+                type={"ionicon"}
+                color={"#fff"}
+                size={36}
+              />
+            </TouchableOpacity>
+          )
+        };
+      }
+    },
+    SalvaBancada: {
+      screen: SBancada,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: "Salva Bancada",
+          headerLeft: null,
+          headerRight: (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Carrinho")}
+              style={{ paddingRight: 10 }}
+            >
+              <Icon
+                name={"ios-cart"}
+                type={"ionicon"}
+                color={"#fff"}
+                size={36}
+              />
+            </TouchableOpacity>
+          )
+        };
+      }
+    },
+    SalvaMetais: {
+      screen: SMetais,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: "Salva Metais",
+          headerLeft: null,
+          headerRight: (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Carrinho")}
+              style={{ paddingRight: 10 }}
+            >
+              <Icon
+                name={"ios-cart"}
+                type={"ionicon"}
+                color={"#fff"}
+                size={36}
+              />
+            </TouchableOpacity>
+          )
+        };
+      }
+    },
+    SalvaPiso: {
+      screen: SP,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: "Salva Piso",
+          headerLeft: null,
+          headerRight: (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Carrinho")}
+              style={{ paddingRight: 10 }}
+            >
+              <Icon
+                name={"ios-cart"}
+                type={"ionicon"}
+                color={"#fff"}
+                size={36}
+              />
+            </TouchableOpacity>
+          )
+        };
+      }
+    },
+    SalvaPisoEspeciais: {
+      screen: SPEspeciais,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: "Salva Piso Especiais",
+          headerLeft: null,
+          headerRight: (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Carrinho")}
+              style={{ paddingRight: 10 }}
+            >
+              <Icon
+                name={"ios-cart"}
+                type={"ionicon"}
+                color={"#fff"}
+                size={36}
+              />
+            </TouchableOpacity>
+          )
+        };
+      }
+    },
+    SalvaPisoPintura: {
+      screen: SPintura,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: "Salva Piso Pintura",
+          headerLeft: null,
+          headerRight: (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Carrinho")}
+              style={{ paddingRight: 10 }}
+            >
+              <Icon
+                name={"ios-cart"}
+                type={"ionicon"}
+                color={"#fff"}
+                size={36}
+              />
+            </TouchableOpacity>
+          )
+        };
+      }
+    },
+    SalvaPisoLimpeza: {
+      screen: SPLimpeza,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: "Salva Piso Limpeza",
+          headerLeft: null,
+          headerRight: (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Carrinho")}
+              style={{ paddingRight: 10 }}
+            >
+              <Icon
+                name={"ios-cart"}
+                type={"ionicon"}
+                color={"#fff"}
+                size={36}
+              />
+            </TouchableOpacity>
+          )
+        };
+      }
+    },
+    SalvaPisoResistente: {
+      screen: SPResistente,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: "Salva Piso Resistente",
+          headerLeft: null,
+          headerRight: (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Carrinho")}
+              style={{ paddingRight: 10 }}
+            >
+              <Icon
+                name={"ios-cart"}
+                type={"ionicon"}
+                color={"#fff"}
+                size={36}
+              />
+            </TouchableOpacity>
+          )
+        };
+      }
+    },
+    SalvaQuina: {
+      screen: SQuina,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: "Salva Quina",
+          headerLeft: null,
+          headerRight: (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Carrinho")}
+              style={{ paddingRight: 10 }}
+            >
+              <Icon
+                name={"ios-cart"}
+                type={"ionicon"}
+                color={"#fff"}
+                size={36}
+              />
+            </TouchableOpacity>
+          )
+        };
+      }
+    },
+    SalvaRalo: {
+      screen: SRalo,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: "Salva Ralo",
           headerLeft: null,
           headerRight: (
             <TouchableOpacity
