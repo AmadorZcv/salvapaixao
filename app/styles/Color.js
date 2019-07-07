c5de525092e6d831725ab9b5948eee79e5bc4073
booude <booude@gmail.com>
2019-07-07 07:48:09 -0300

    Orientação de cores

--- app/styles/Color.js
+++ app/styles/Color.js
@@ -4,3 +4,35 @@ const Color = {
 };
 
 export default Color;
+
+//#Screen Background
+  const backgroundColor = {
+    primary: "rgba(0, 0, 98, 1)"
+  }
+  //Background Bar para diferenciação de itens do Cart.js
+  const lightBarColor = {
+    primary: "rgba(0, 0, 93, 1)"
+  }
+  //Background Bar para diferenciação nas Telas 2 e Info Bar do Cart.js
+  const darkBarColor = {
+    primary: "rgba(0, 0, 89, 1)"
+  }
+
+//#Header
+  const headerColor = {
+    primary: "rgba(0, 0, 96, 1)"
+  }
+
+//#Font Color Scheme
+  //Para as fontes escuras
+  const primaryFonts = {
+    primary: "rgba(0, 0, 0, 0.87)"
+  };
+  //Para as fontes claras e SVGs, Ícones
+  const secondaryFonts = {
+    primary: "rgba(0, 0, 0, 0.54)"
+  };
+  //Caso seja necessária alguma função posterior
+  const disabledFonts = {
+    primary: "rgba(0, 0, 0, 0.38)"
+  };
