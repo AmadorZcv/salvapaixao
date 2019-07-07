18f3da2a0abe91e3feb3d255aa49e453b52f7153
AmadorZcv <lucasamadorzcv@gmail.com>
2019-07-06 23:12:30 -0300

    dados formatados

--- app/redux/reducers.js
+++ app/redux/reducers.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
 import cart from "./cart/reducer";
+import data from "./products/data";
+console.log("Data é", data);
+
 export default combineReducers({
   cart
 });
