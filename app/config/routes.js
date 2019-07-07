fb5e6976ff4043614782618db466ea07a8f3b884
AmadorZcv <lucasamadorzcv@gmail.com>
2019-07-06 21:05:29 -0300

    Tela inicial

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,76 +1,62 @@
-import React from 'react'
-
+import React from "react";
+import { TouchableOpacity } from "react-native";
 import {
   createBottomTabNavigator,
   createAppContainer,
   createStackNavigator
-} from 'react-navigation'
-import Home from '../screens/Home'
-import { Icon } from 'react-native-elements'
-import { Color, TextStyle } from '../styles'
+} from "react-navigation";
+import Home from "../screens/Home";
+import { Icon } from "react-native-elements";
+import { Color, TextStyle } from "../styles";
 
 const HomeStack = createStackNavigator(
-    {
-      Home: {
-        screen: Home,
-        navigationOptions: ({ navigation }) => {
-          return {
-            headerTitle: 'Home',
-            headerLeft: null,
-            headerRight: null
-          }
-        }
-      }
-    },
-    {
-      /* The header config from HomeScreen is now here */
-      defaultNavigationOptions: {
-        headerStyle: {
-          backgroundColor: Color.primary
-        },
-        headerTitleStyle: {
-          ...TextStyle.header,
-          alignSelf: 'center',
-          textAlign: 'center',
-          justifyContent: 'center',
-          flex: 1
-        },
-        headerTintColor: Color.white
-      }
-    }
-  )
-const TabNavigator = createBottomTabNavigator(
-    {
-      Home: HomeStack,
-    },
-    {
-      defaultNavigationOptions: ({ navigation }) => ({
-        tabBarIcon: ({ focused, horizontal, tintColor }) => {
-          const { routeName } = navigation.state
-          let iconName
-          if (routeName === 'Home') {
-            iconName = `book-open`
-            // Sometimes we want to add badges to some icons.
-            // You can check the implementation below.
-          }
-          // You can return any component that you like here!
-          return (
-            <Icon
-              name={iconName}
-              size={25}
-              color={tintColor}
-              type={'simple-line-icon'}
-            />
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: "Salva Paixão",
+          headerLeft: null,
+          headerRight: (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Carrinho")}
+              style={{ paddingRight: 10 }}
+            >
+              <Icon
+                name={"ios-cart"}
+                type={"ionicon"}
+                color={"#fff"}
+                size={36}
+              />
+            </TouchableOpacity>
           )
-        }
-      }),
-      tabBarOptions: {
-        activeTintColor: Color.white,
-        inactiveTintColor: 'gray',
-        activeBackgroundColor: Color.primary,
-        inactiveBackgroundColor: Color.primary
+        };
+      }
+    },
+    Carrinho: {
+      screen: Home,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerTitle: "Carrinho"
+        };
       }
     }
-  )
-  
-  export default createAppContainer(TabNavigator)
+  },
+  {
+    /* The header config from HomeScreen is now here */
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Color.primary
+      },
+      headerTitleStyle: {
+        ...TextStyle.header,
+        alignSelf: "center",
+        textAlign: "center",
+        justifyContent: "center"
+      },
+      headerTintColor: Color.white
+    }
+  }
+);
+
+export default createAppContainer(HomeStack);
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,18 +1,84 @@
-import React, { PureComponent } from 'react';
-import {  View, Text } from 'react-native';
+import React, { PureComponent } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { Image } from "react-native-elements";
 
 export default class Home extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
     return (
-      <View>
-        <Text> componentText </Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={{ ...styles.cardLine, marginTop: 5 }}>
+          <Image
+            source={require("../img/index/spt-logo.png")}
+            containerStyle={styles.card}
+          />
+          <Image
+            source={require("../img/index/spm-logo.png")}
+            containerStyle={styles.card}
+          />
+        </View>
+        <View style={styles.cardLine}>
+          <Image
+            source={require("../img/index/spr-logo.png")}
+            containerStyle={styles.card}
+          />
+          <Image
+            source={require("../img/index/spl-logo.png")}
+            containerStyle={styles.card}
+          />
+        </View>
+        <View style={styles.cardLine}>
+          <Image
+            source={require("../img/index/spin-logo.png")}
+            containerStyle={styles.card}
+          />
+          <Image
+            source={require("../img/index/sq-logo.png")}
+            containerStyle={styles.card}
+          />
+        </View>
+        <View style={styles.cardLine}>
+          <Image
+            source={require("../img/index/sm-logo.png")}
+            containerStyle={styles.card}
+          />
+          <Image
+            source={require("../img/index/sb-logo.png")}
+            containerStyle={styles.card}
+          />
+        </View>
+        <View style={styles.cardLine}>
+          <Image
+            source={require("../img/index/sr-logo.png")}
+            containerStyle={styles.card}
+          />
+          <Image
+            source={require("../img/index/smb-logo.png")}
+            containerStyle={styles.card}
+            resizeMode={"stretch"}
+            style={{ width: 170 }}
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  cardLine: {
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  card: {
+    margin: 5,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e6e7e9"
+  }
+});
