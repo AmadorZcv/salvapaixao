d271ac903350110f7d76e98bec20803a96af53bc
AmadorZcv <lucasamadorzcv@gmail.com>
2019-07-09 03:52:59 -0300

    Retornando para o on press antigo

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -1,27 +1,10 @@
 import React, { PureComponent } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
-import { Icon, Image } from "react-native-elements";
+import { Image } from "react-native-elements";
 import { contadorText } from "../styles/Text";
 
 export default class Counter extends PureComponent {
-  constructor() {
-    super();
-    this.timer = null;
-  }
-
-  addOne = () => {
-    this.props.onPlus();
-    this.timer = setTimeout(this.addOne, 300);
-  };
-  minusOne = () => {
-    this.props.onMinus();
-    this.timer = setTimeout(this.minusOne, 300);
-  };
-
-  stopTimer = () => {
-    clearTimeout(this.timer);
-  };
   render() {
     const { total, onMinus, onPlus } = this.props;
 
@@ -33,22 +16,14 @@ export default class Counter extends PureComponent {
           justifyContent: "center"
         }}
       >
-        <TouchableOpacity
-          //onPress={onMinus}
-          onPressIn={this.minusOne}
-          onPressOut={this.stopTimer}
-        >
+        <TouchableOpacity onPress={onMinus}>
           <Image
             source={require("../img/icons/minus.png")}
             style={{ width: 24, height: 24 }}
           />
         </TouchableOpacity>
         <Text style={contadorText}> {total} </Text>
-        <TouchableOpacity
-          //onPress={onPlus}
-          onPressIn={this.addOne}
-          onPressOut={this.stopTimer}
-        >
+        <TouchableOpacity onPress={onPlus}>
           <Image
             source={require("../img/icons/plus.png")}
             style={{ width: 24, height: 24 }}
