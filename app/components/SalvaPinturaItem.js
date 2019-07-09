6dd2e771b008843c2fd5d8d85c0c7f74e6b07680
AmadorZcv <lucasamadorzcv@gmail.com>
2019-07-09 02:21:05 -0300

    Salva Pintura Item clean

--- app/components/SalvaPinturaItem.js
+++ app/components/SalvaPinturaItem.js
@@ -1,122 +1,66 @@
 import React, { PureComponent } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text } from "react-native";
 import { integerToReal } from "../config/formatUtils";
-import { Icon } from "react-native-elements";
-import { Color } from "../styles";
+
+import { textInfo, textValue } from "../styles/Text";
+import PrecoCustoCounter from "./PrecoCustoCounter";
 
 export default class SalvaPinturaItem extends PureComponent {
-    render() {
-        const {
-            index,
-            altura,
-            comprimento,
-            cobertura,
-            preco,
-            ipi,
-            ipiR,
-            total
-        } = this.props;
-        const trueTotal = total !== undefined ? total.qtd : 0;
-        const precoTotal = preco + ipiR;
-        const precoFinal = precoTotal * trueTotal;
-        const formatPrecoFinal = precoFinal > 0 ? integerToReal(precoFinal) : "0";
-        const backgroundColor = index % 2 === 0 ? "white" : "lightgray";
-        return (
-            <View
-                style={{
-                    flexDirection: "row",
-                    backgroundColor,
-                    flex: 1,
-                    paddingLeft: 7
-                }}
-            >
-                <View style={{ flexDirection: "row" }}>
-                    <View style={styles.column}>
-                        <Text style={styles.textInfo}>Altura</Text>
-                        <Text style={styles.textInfo}>(Metros)</Text>
-                        <Text style={styles.textValue}>{altura}</Text>
-                        <Text style={styles.textInfo}>Preço</Text>
-                        <Text style={styles.textInfo}>(ICMS)</Text>
-                        <Text style={styles.textValue}>R$ {integerToReal(preco)}</Text>
-                    </View>
-                </View>
-                <View style={{ flexDirection: "row" }}>
-                    <View style={styles.column}>
-                        <Text style={styles.textInfo}>Comprimento</Text>
-                        <Text style={styles.textInfo}>(Metros)</Text>
-                        <Text style={styles.textValue}>{comprimento}</Text>
-                        <Text style={styles.textInfo}>IPI</Text>
-                        <Text style={styles.textInfo}>(%)</Text>
-                        <Text style={styles.textValue}>{ipi}</Text>
-                    </View>
-                </View>
-                <View style={{ flexDirection: "row" }}>
-                    <View style={styles.column}>
-                        <Text style={styles.textInfo}>Cobertura</Text>
-                        <Text style={styles.textInfo}>(Metros²)</Text>
-                        <Text style={styles.textValue}>{cobertura}</Text>
-                        <Text style={styles.textInfo}>IPI</Text>
-                        <Text style={styles.textInfo}>(R$)</Text>
-                        <Text style={styles.textValue}>R$ {integerToReal(ipiR)}</Text>
-                    </View>
-                </View>
+  render() {
+    const {
+      index,
+      altura,
+      comprimento,
+      cobertura,
+      preco,
+      ipi,
+      ipiR,
+      total
+    } = this.props;
+    const trueTotal = total !== undefined ? total.qtd : 0;
+    const precoTotal = preco + ipiR;
+    const precoFinal = precoTotal * trueTotal;
+    const formatPrecoFinal = precoFinal > 0 ? integerToReal(precoFinal) : "0";
+    const backgroundColor = index % 2 === 0 ? "white" : "lightgray";
+    return (
+      <View style={{ ...itemContainer, backgroundColor }}>
+        <View style={columnContainer}>
+          <View>
+            <Text style={textInfo}>Altura</Text>
+            <Text style={textInfo}>(Metros)</Text>
+            <Text style={textValue}>{altura}</Text>
+            <Text style={textInfo}>Preço</Text>
+            <Text style={textInfo}>(ICMS)</Text>
+            <Text style={textValue}>R$ {integerToReal(preco)}</Text>
+          </View>
 
-                <View style={{ flex: 1 }}>
-                    <Text style={styles.textPreco}>R$ {integerToReal(precoTotal)}</Text>
-                    <Text style={styles.textCusto}>Custo Total:</Text>
-                    <Text style={styles.textValueFinal}>R$ {formatPrecoFinal}</Text>
-                    <View
-                        style={{
-                            flexDirection: "row",
-                            marginTop: 5, justifyContent: "center"
-                        }}
-                    >
-                        <TouchableOpacity onPress={this.props.onMinus}>
-                            <Icon name={"minus"} type={"feather"} />
-                        </TouchableOpacity>
-                        <Text style={styles.textPreco}> {trueTotal} </Text>
-                        <TouchableOpacity onPress={this.props.onPlus}>
-                            <Icon name={"plus"} type={"feather"} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-        );
-    }
+          <View>
+            <Text style={textInfo}>Comprimento</Text>
+            <Text style={textInfo}>(Metros)</Text>
+            <Text style={textValue}>{comprimento}</Text>
+            <Text style={textInfo}>IPI</Text>
+            <Text style={textInfo}>(%)</Text>
+            <Text style={textValue}>{ipi}</Text>
+          </View>
+
+          <View>
+            <Text style={textInfo}>Cobertura</Text>
+            <Text style={textInfo}>(Metros²)</Text>
+            <Text style={textValue}>{cobertura}</Text>
+            <Text style={textInfo}>IPI</Text>
+            <Text style={textInfo}>(R$)</Text>
+            <Text style={textValue}>R$ {integerToReal(ipiR)}</Text>
+          </View>
+        </View>
+        <View style={{ flex: 1 }} />
+        <PrecoCustoCounter
+          onPlus={this.props.onPlus}
+          onMinus={this.props.onMinus}
+          total={trueTotal}
+          precoTotal={precoTotal}
+          precoFinal={formatPrecoFinal}
+        />
+      </View>
+    );
+  }
 }
-const styles = StyleSheet.create({
-    column: {
-        marginHorizontal: 5
-    },
-    textValue: {
-        color: Color.secondaryText,
-        textAlign: "center",
-        paddingTop: 3,
-        fontSize: 10
-    },
-    textInfo: {
-        textAlign: "center",
-        color: Color.primaryText,
-        fontWeight: "600",
-        fontSize: 10
-    },
-    textPreco: {
-        fontSize: 15,
-        color: Color.secondaryText,
-        textAlign: "center",
-        fontWeight: "100",
-        marginBottom: 5
-    },
-    textCusto: { fontSize: 12, textAlign: "center", color: Color.primaryText },
-    textValueFinal: {
-        fontSize: 16,
-        textAlign: "center",
-        color: Color.primaryText
-    },
-    contadorText: {
-        fontSize: 16,
-        color: Color.secondaryText,
-        textAlign: "center",
-        marginBottom: 5
-    }
-});
