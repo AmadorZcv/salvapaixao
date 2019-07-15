4d13aa1d921c67c10a50f927915a11bfdb06dd70
AmadorZcv <lucasamadorzcv@gmail.com>
2019-07-15 04:41:43 -0300

    Salva quina ok

--- app/components/SalvaQuinaItem.js
+++ app/components/SalvaQuinaItem.js
@@ -1,16 +1,72 @@
 import React, { PureComponent } from "react";
 import { View, Text } from "react-native";
+import { itemContainer, columnContainer } from "../styles/Containers";
+import { integerToReal } from "../config/formatUtils";
+import PrecoCustoCounter from "./PrecoCustoCounter";
+import { textInfo, textValue } from "../styles/Text";
 
 export default class SalvaQuinaItem extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
+    const { index, altura, comprimento, preco, ipi, ipiR, total } = this.props;
+    const trueTotal = total !== undefined ? total.qtd : 0;
+    const precoTotal = preco + ipiR;
+    const precoFinal = precoTotal * trueTotal;
+    const formatPrecoFinal = precoFinal > 0 ? integerToReal(precoFinal) : "0";
+    const backgroundColor = index % 2 === 0 ? "white" : "lightgray";
     return (
-      <View>
-        <Text> componentText </Text>
+      <View style={{ ...itemContainer, backgroundColor }}>
+        <View style={{ ...columnContainer, flexDirection: "column" }}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-around"
+            }}
+          >
+            <View>
+              <Text style={textInfo}>Altura</Text>
+              <Text style={textInfo}>(Centímetros)</Text>
+              <Text style={textValue}>{altura}</Text>
+            </View>
+            <View>
+              <Text style={textInfo}>Comprimento</Text>
+              <Text style={textInfo}>(Centímetros)</Text>
+              <Text style={textValue}>{comprimento}</Text>
+            </View>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-between"
+            }}
+          >
+            <View>
+              <Text style={textInfo}>Preço</Text>
+              <Text style={textInfo}>(ICMS)</Text>
+              <Text style={textValue}>R$ {integerToReal(preco)}</Text>
+            </View>
+            <View>
+              <Text style={textInfo}>IPI</Text>
+              <Text style={textInfo}>(%)</Text>
+              <Text style={textValue}>{ipi}</Text>
+            </View>
+            <View>
+              <Text style={textInfo}>IPI</Text>
+              <Text style={textInfo}>(R$)</Text>
+              <Text style={textValue}>R$ {integerToReal(ipiR)}</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ flex: 1 }} />
+        <PrecoCustoCounter
+          onPlus={this.props.onPlus}
+          onMinus={this.props.onMinus}
+          onChange={this.props.onChange}
+          total={trueTotal}
+          precoTotal={precoTotal}
+          precoFinal={formatPrecoFinal}
+        />
       </View>
     );
   }
--- app/screens/SQuina.js
+++ app/screens/SQuina.js
@@ -1,17 +1,50 @@
 import React, { PureComponent } from "react";
-import { View, Text } from "react-native";
-
-export default class SQuina extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
+import { Text, Image, ScrollView, Dimensions } from "react-native";
+import SalvaPisoItem from "../components/SalvaPisoItem";
+import { connect } from "react-redux";
+import { addToCart, removeFromCart, setQtdCart } from "../redux/cart/actions";
+import { productLabel } from "../styles/Text";
+import SalvaRaloItem from "../components/SalvaRaloItem";
+import SalvaQuinaItem from "../components/SalvaQuinaItem";
+class SQuina extends PureComponent {
+  onPlus = id => {
+    this.props.dispatch(addToCart(id));
+  };
+  onMinus = id => {
+    this.props.dispatch(removeFromCart(id));
+  };
+  onChange = (qtd, id) => {
+    this.props.dispatch(setQtdCart(qtd, id));
+  };
   render() {
+    const { products, cart } = this.props;
+    const { width } = Dimensions.get;
     return (
-      <View>
-        <Text> componentText </Text>
-      </View>
+      <ScrollView style={{ flex: 1 }}>
+        <Image
+          source={require("../img/product-screen/s-quina-top.png")}
+          style={{ width, height: 190 }}
+        />
+        <Text style={productLabel}>Proteção para Quinas e Batentes</Text>
+        <SalvaQuinaItem
+          index={0}
+          altura={products["15000"].largura}
+          comprimento={products["15000"].comprimento}
+          preco={products["15000"].preco}
+          ipi={products["15000"].ipi}
+          ipiR={products["15000"].ipic}
+          onPlus={() => this.onPlus("15000")}
+          onMinus={() => this.onMinus("15000")}
+          onChange={qtd => this.onChange(qtd, "15000")}
+          total={cart["15000"]}
+        />
+      </ScrollView>
     );
   }
 }
+const mapStateToProps = state => {
+  const { products } = state.products;
+  const { cart } = state.cart;
+  return { products, cart };
+};
+export default connect(mapStateToProps)(SQuina);
