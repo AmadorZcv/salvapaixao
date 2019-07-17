794326338059beb703cca23ddaa4e09303b7b295
booude <booude@gmail.com>
2019-07-17 00:04:32 -0300

    Diferenciador de milhar (Coloquei espaço)

--- app/config/formatUtils.js
+++ app/config/formatUtils.js
@@ -1,8 +1,18 @@
 export function integerToReal(value) {
   const string = value.toString();
-  const formatted =
-    string.slice(0, string.length - 2) +
-    "," +
-    string.slice(string.length - 2, string.length);
-  return formatted;
+  if (string.length > 5) {
+    const formatted =
+      string.slice(0, string.length - 5) +
+      " " +
+      string.slice(string.length - 5, string.length - 2) +
+      "," +
+      string.slice(string.length - 2, string.length);
+    return formatted;
+  } else {
+    const formatted =
+      string.slice(0, string.length - 2) +
+      "," +
+      string.slice(string.length - 2, string.length);
+    return formatted;
+  }
 }
