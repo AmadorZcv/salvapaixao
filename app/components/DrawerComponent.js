01c61d2ad049350843735948d21a460aac75ecf1
AmadorZcv <lucasamadorzcv@gmail.com>
2019-07-26 02:36:20 -0300

    Menu ok

--- app/components/DrawerComponent.js
+++ app/components/DrawerComponent.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { ScrollView, StyleSheet, Animated, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Animated,
+  View,
+  ImageBackground
+} from "react-native";
+import VersionNumber from "react-native-version-number";
 import { DrawerItems, SafeAreaView } from "react-navigation";
-import { Text } from "react-native-elements";
+import { Text, Avatar, Image } from "react-native-elements";
 import { Color } from "../styles";
 
 export const CustomDrawerContentComponent = props => {
@@ -16,18 +23,75 @@ export const CustomDrawerContentComponent = props => {
           style={styles.container}
           forceInset={{ top: "always", horizontal: "never" }}
         >
-          <View>
-            <Text>Perfil</Text>
+          <View
+            style={{
+              backgroundColor: "#7C7C7C"
+            }}
+          >
+            <View style={{ flexDirection: "row", height: 50 }}>
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: "flex-end",
+                  alignItems: "flex-start",
+                  paddingLeft: 30
+                }}
+              >
+                <Avatar
+                  source={require("../img/icons/home.png")}
+                  size={"small"}
+                  rounded
+                />
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: "flex-start",
+                  alignItems: "flex-end",
+                  paddingRight: 5
+                }}
+              >
+                <Image
+                  source={require("../img/logo/menu-ntp-logo.png")}
+                  style={{
+                    width: 34,
+                    height: 34
+                  }}
+                  resizeMode={"contain"}
+                />
+              </View>
+            </View>
+            <View
+              style={{
+                height: 70,
+                width: 240,
+                justifyContent: "center",
+                alignItems: "flex-start",
+                paddingLeft: 30,
+                backgroundColor: "#7C7C7C"
+              }}
+            >
+              <Text style={{ color: Color.white }}>Daniel</Text>
+              <Text style={{ color: Color.white }}>Gerente de vendas</Text>
+            </View>
           </View>
-          <View>
-            <Text>Icones</Text>
-          </View>
-          <View>
-            <Text>Final</Text>
-          </View>
-          <Text>Salva Brás</Text>
           <DrawerItems {...props} />
-          <Text style={{ flex: 1, justifyContent: "flex-end" }}>Sair</Text>
+          <View style={{ marginTop: 100 }}>
+            <Image
+              source={require("../img/icons/qr-icon.png")}
+              style={{ opacity: 0.5, alignSelf: "center" }}
+            />
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-around" }}
+            >
+              <Text>Sair</Text>
+              <Text>Sobre</Text>
+            </View>
+            <Text style={{ textAlign: "center" }}>Salva Compra</Text>
+            <Text style={{ textAlign: "center" }}>
+              Versão {VersionNumber.appVersion}
+            </Text>
+          </View>
         </SafeAreaView>
       </ScrollView>
     </Animated.View>
