09df6e5f8a075a15ca106b2a38b196d8d361b517
AmadorZcv <lucasamadorzcv@gmail.com>
2019-07-25 23:07:48 -0300

    Inicio contador de itens no carrinho

--- app/components/header/CarrinhoIcon.js
+++ app/components/header/CarrinhoIcon.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from "react";
-
-import { Image } from "react-native-elements";
+import { View, ImageBackground } from "react-native";
+import { Image, Text } from "react-native-elements";
+import { Color } from "../../styles";
 
 export default class CarrinhoIcon extends PureComponent {
   constructor(props) {
@@ -10,7 +11,7 @@ export default class CarrinhoIcon extends PureComponent {
 
   render() {
     return (
-      <Image
+      <ImageBackground
         style={{
           height: 22,
           width: 22,
@@ -18,7 +19,31 @@ export default class CarrinhoIcon extends PureComponent {
         }}
         source={require("../../img/icons/shopping-cart.png")}
         resizeMode={"contain"}
-      />
+      >
+        <View
+          style={{
+            backgroundColor: "#FE3A3A",
+            position: "absolute",
+            right: 10,
+            bottom: 0,
+            borderRadius: 7.5,
+            minHeight: 15,
+            minWidth: 15,
+            justifyContent: "center"
+          }}
+        >
+          <Text
+            style={{
+              color: Color.white,
+              fontSize: 10,
+              textAlign: "center",
+              textAlignVertical: "center"
+            }}
+          >
+            99
+          </Text>
+        </View>
+      </ImageBackground>
     );
   }
 }
