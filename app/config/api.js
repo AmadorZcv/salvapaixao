0726a1cff2804c4559697d7398a5e657cac54b66
AmadorZcv <lucasamadorzcv@gmail.com>
2019-09-02 01:29:13 -0300

    Voltando api pro host certo

--- app/config/api.js
+++ app/config/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-const hostUrl = "http://b4c50280.ngrok.io/";
-//const hostUrl = "https://fathomless-hamlet-33662.herokuapp.com";
+//const hostUrl = "http://192.168.0.102:4000"
+//const hostUrl = "http://b4c50280.ngrok.io/";
+const hostUrl = "https://fathomless-hamlet-33662.herokuapp.com";
 
 const defaultOptions = {
   baseURL: hostUrl,
